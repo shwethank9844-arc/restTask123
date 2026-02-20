848493a2ceba7631731b328eb0c4fd2d89ddb2e7
shwethank9844-arc <shwethank9844@gmail.com>
2026-02-20 15:51:33 +0530

    taskkkkk

--- src/test/java/PaginationTable/Dynamic.java
+++ src/test/java/PaginationTable/Dynamic.java
@@ -1,39 +1,14 @@
 package PaginationTable;
 
 import java.time.Duration;
-import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
-public class AmazonTask {
-
-	@Test
-	public void task() throws InterruptedException {
-		 WebDriver driver=new ChromeDriver();
-    	 driver.manage().window().maximize();
-    	 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-    	 
-    	 driver.get("https://www.amazon.in/");
-    	 Actions act=new Actions(driver);
-    	 
-    	 
-    	 WebElement ele = driver.findElement(By.xpath("//div[@id='icp-nav-flyout']/descendant::button[@aria-label='Expand to Change Language or Country']"));
-    	 Thread.sleep(2000);
-    	 act.moveToElement(ele).click().perform();
-    	 
-    	  List<WebElement> eles = driver.findElements(By.xpath("//div[@id='nav-flyout-icp']/descendant::ul/li/a/span/i"));
-    	  for(WebElement win:eles) {
-    		  win.click();
-    	  }
-    	  
-    	  System.out.println("asdfghjkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk11111111111111111111111111");
-	}
-	
+public class Dynamic {
 	@Test
 	public void task3() throws InterruptedException {
 		
